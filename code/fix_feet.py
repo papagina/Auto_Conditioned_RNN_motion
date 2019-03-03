fbb7f6414035fc5c6752e1c21b8d8c1f777b0eb8
Yi_Zhou <zhouyisjtu2012@gmail.com>
2019-03-03 14:49:45 -0800

    Update fix_feet.py

--- code/fix_feet.py
+++ code/fix_feet.py
@@ -1,10 +1,11 @@
 
-from read_bvh import *
+import read_bvh
 import numpy as np
 
 
-bvh_file = '../test_bvh_aclstm_indian/out00.bvh'
+bvh_file = 'xx.bvh'
 train_data=read_bvh.get_train_data(bvh_file)
+train_data = train_data[1:]
 num_frames = train_data.shape[0]
 
 joint_index = read_bvh.joint_index
@@ -14,31 +15,52 @@ joint_index = read_bvh.joint_index
 hip_idx = 3 * joint_index['hip']
 lfn_idx, rfn_idx = 3 * joint_index['lFoot_Nub'], 3 * joint_index['rFoot_Nub']
 
-last_feet_pos = (train_data[1][lfn_idx:lfn_idx + 3], train_data[1][rfn_idx:rfn_idx + 3])
-last_hip_pos = train_data[1][hip_idx:hip_idx + 3]
-for i in xrange(2, num_frames):
-    feet_pos = (train_data[i][lfn_idx:lfn_idx + 3], train_data[i][rfn_idx:rfn_idx + 3])
-    hip_pos = train_data[i][hip_idx:hip_idx + 3]
-    abs_lower_foot = feet_pos[lower_foot]+hip_pos
-    if(abs_lower_foot[1]<0.01):
-        set = (last_feet_pos[lower_foot] + last_hip_pos) - (feet_pos[lower_foot] + hip_pos)
-        offset[1] = 0
-        #train_data[i][hip_idx:hip_idx + 3] = train_data[i - 1][hip_idx:hip_idx + 3] + (hip_pos - last_hip_pos) + offset
-        train_data[i][hip_idx:hip_idx + 3] += offset
-        last_feet_pos = feet_pos
-        #last_hip_pos = hip_pos
-        last_hip_pos = train_data[i][hip_idx:hip_idx + 3]
-	
-for i in xrange(2, num_frames):
-	feet_pos = (train_data[i][lfn_idx:lfn_idx + 3], train_data[i][rfn_idx:rfn_idx + 3])
-	hip_pos = train_data[i][hip_idx:hip_idx + 3]
-	lower_foot = (int)(feet_pos[0][1] > feet_pos[1][1])
-	offset = np.zeros(3)
-	abs_lower_foot = feet_pos[lower_foot]+hip_pos
-	if (abs_lower_foot[1] < 0):
-		offset[1]=-abs_lower_foot[1]  
-	train_data[i][hip_idx:hip_idx + 3] += offset
+print ("Find the ground level.")
+global_feet_y =np.concatenate(( train_data[:, lfn_idx+1]+train_data[:, hip_idx+1 ], train_data[:, rfn_idx+1]+train_data[:, hip_idx+1 ]), 0)
+global_feet_y=np.sort(global_feet_y)
+ground_level = global_feet_y[0:int(num_frames/4)].mean()
+
+print ("The ground level is: "+str(ground_level))
+
+print ("Move the ground to 0.")
+train_data[:, hip_idx+1 ] +=-ground_level
+
+
+print ("Solve feet sliding.")
+
+last_feet_pos = (train_data[0, lfn_idx:lfn_idx + 3], train_data[0, rfn_idx:rfn_idx + 3])
+
+hip_pos_v = train_data[1:, hip_idx:hip_idx+3] - train_data[0:num_frames-1, hip_idx:hip_idx+3] ##difference of hips between frames. 
+is_last_frame_feet_on_ground = [0,0] # left, right
+for i in range(1, num_frames-1):
+    hip_pos = train_data[i, hip_idx:hip_idx + 3]
+    feet_pos = (train_data[i,lfn_idx:lfn_idx + 3], train_data[i,rfn_idx:rfn_idx + 3])
+    lower_foot_idx = (int)(feet_pos[0][1] > feet_pos[1][1])
+    global_lower_foot = feet_pos[lower_foot_idx]+hip_pos
+
+    if(global_lower_foot[1]<0.01): ##If touches the ground
+        if(is_last_frame_feet_on_ground[lower_foot_idx] == 1):
+            offset = (last_feet_pos[lower_foot_idx] - feet_pos[lower_foot_idx]) 
+            hip_pos_v[i-1,0] =offset[0]
+            hip_pos_v[i-1,2] =offset[2]
+        is_last_frame_feet_on_ground=[0,0]
+        is_last_frame_feet_on_ground[lower_foot_idx]=1
+        print ("fix frame "+str(i))
+    else:
+        is_last_frame_feet_on_ground=[0,0]
+        
+    last_feet_pos = feet_pos
+
+for i in range(1, num_frames):   
+    train_data[i,hip_idx:hip_idx+3] = train_data[i-1,hip_idx:hip_idx+3] + hip_pos_v[i-1] 
     
 
+    
+print ("write bvh")
 
-read_bvh.write_traindata_to_bvh('../test_bvh_aclstm_indian/out00_feetfixed.bvh', train_data)
+read_bvh.write_traindata_to_bvh('xx_fixed.bvh', train_data)
+
+
+
+
+
